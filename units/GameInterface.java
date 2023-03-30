2017129efccd7af9c98f5f983dac9e3c528676e9
Yaroslav <Valso96@mail.ru>
2023-03-30 10:43:16 +0600

    added progress 1

---
+++ units/GameInterface.java
@@ -0,0 +1,9 @@
+package seminar1.units;
+
+public interface GameInterface {
+
+    void step();
+
+    String getInfo();
+
+}
--- units/Peasant.java
+++ units/Peasant.java
@@ -3,6 +3,11 @@ package seminar1.units;
 public class Peasant extends BaseHero {
 
     public Peasant(String name) {
-        super(200, name, 1, 6, 20, new int[] { 3, 5 }, "Крестьянин");
+        super(200, name, 1, 6, 20, new int[]{3, 5});
+    }
+
+    @Override
+    public String getInfo() {
+        return "Крестьянин";
     }
 }
