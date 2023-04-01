90cd38c4052edf7812231d4d0853db732bb87d91
Yaroslav <Valso96@mail.ru>
2023-04-01 11:01:52 +0600

    added progress 2

--- units/GameInterface.java
+++ units/GameInterface.java
@@ -1,8 +1,10 @@
 package seminar1.units;
 
+import java.util.ArrayList;
+
 public interface GameInterface {
 
-    void step();
+    void step(ArrayList<BaseHero> enemyTeam);
 
     String getInfo();
 
--- units/Peasant.java
+++ units/Peasant.java
@@ -2,12 +2,12 @@ package seminar1.units;
 
 public class Peasant extends BaseHero {
 
-    public Peasant(String name) {
-        super(200, name, 1, 6, 20, new int[]{3, 5});
+    public Peasant(String name, boolean firstTeam) {
+        super(200, name, firstTeam, 20, new int[]{3, 5});
     }
 
     @Override
     public String getInfo() {
-        return "Крестьянин";
+        return "Крестьянин " + name;
     }
 }
